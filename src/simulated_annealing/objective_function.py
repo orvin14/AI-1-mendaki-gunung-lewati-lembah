3dc335fcfda2100582f8c6ce645e54da9ec35762
KarRz <reza11781@gmail.com>
2025-10-16 12:31:09 +0700

    fix: adjust conflict calcalation

--- src/simulated_annealing/objective_function.py
+++ src/simulated_annealing/objective_function.py
@@ -1,3 +1,10 @@
+def bobot_penalti(prioritas):
+    match prioritas:
+        case 1: return 1.75
+        case 2: return 1.5
+        case 3: return 1.25
+        case _: return 1.0
+
 def objective_function(population, kelas_mata_kuliah, ruangan, mahasiswa):
     conflict = 0
     for i in range(len(mahasiswa)):
@@ -8,7 +15,7 @@ def objective_function(population, kelas_mata_kuliah, ruangan, mahasiswa):
                 if jadwal_mahasiswa[j]['hari'] == jadwal_mahasiswa[k]['hari']:
                     if not (jadwal_mahasiswa[j]['waktu_selesai'] <= jadwal_mahasiswa[k]['waktu_mulai'] or
                             jadwal_mahasiswa[k]['waktu_selesai'] <= jadwal_mahasiswa[j]['waktu_mulai']):
-                        conflict += 10
+                        conflict += 2
                         # print(f"Conflict for student {mahasiswa[i]['nim']} between {jadwal_mahasiswa[j]['kode']} and {jadwal_mahasiswa[k]['kode']}")
     for i in range(len(population)):
         sesi1 = population[i]
@@ -16,7 +23,12 @@ def objective_function(population, kelas_mata_kuliah, ruangan, mahasiswa):
             sesi2 = population[j]
             if sesi1['ruangan'] == sesi2['ruangan'] and sesi1['hari'] == sesi2['hari']:
                 if not (sesi1['waktu_selesai'] <= sesi2['waktu_mulai'] or sesi2['waktu_selesai'] <= sesi1['waktu_mulai']):
-                    conflict += 10
+                    for siswa in mahasiswa:
+                        if sesi1['kode'] in siswa['daftar_mk']:
+                            conflict += (sesi1['waktu_selesai'] - sesi1['waktu_mulai'])*bobot_penalti(siswa['prioritas'][siswa['daftar_mk'].index(sesi1['kode'])])
+                    for siswa in mahasiswa:
+                        if sesi2['kode'] in siswa['daftar_mk']:
+                            conflict += (sesi2['waktu_selesai'] - sesi2['waktu_mulai'])*bobot_penalti(siswa['prioritas'][siswa['daftar_mk'].index(sesi2['kode'])])
                     # print(f"Conflict in room {sesi1['ruangan']} between {sesi1['kode']} and {sesi2['kode']}")
     for i in range(len(population)):
         sesi = population[i]
@@ -29,6 +41,6 @@ def objective_function(population, kelas_mata_kuliah, ruangan, mahasiswa):
             if ruang['kode'] == sesi['ruangan']:
                 ruangan_sesi = ruang
         if kelas_sesi['jumlah_mahasiswa'] > ruangan_sesi['kuota']:
-            conflict += (kelas_sesi['jumlah_mahasiswa'] - ruangan_sesi['kuota'])
+            conflict += (kelas_sesi['jumlah_mahasiswa'] - ruangan_sesi['kuota'])*(sesi['waktu_selesai'] - sesi['waktu_mulai'])
             # print(f"Capacity conflict in room {sesi['ruangan']} for class {sesi['kode']}")
     return conflict
