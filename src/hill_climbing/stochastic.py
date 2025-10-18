52cc97b1b80b29e7faa9644f71f003af0baeb8d2
KarRz <reza11781@gmail.com>
2025-10-19 00:41:35 +0700

    fix: update stoping logic

--- src/hill_climbing/stochastic.py
+++ src/hill_climbing/stochastic.py
@@ -1,5 +1,3 @@
-import math
-import random
 import copy
 from simulated_annealing.objective_function import objective_function
 from simulated_annealing.generate_neighbor import generate_neighbor
@@ -15,10 +13,11 @@ def stochastic_hill_climbing(initial_state, kelas_mata_kuliah, ruangan, mahasisw
         neighbor = generate_neighbor(current_state, ruangan, hari)
         neighbor_penalty = objective_function(neighbor, kelas_mata_kuliah, ruangan, mahasiswa)
         
-        if current_penalty == 0:
-            
+        if  neighbor_penalty == 0:
+            history['iterasi'].append(iterasi_total)
+            history['penalti'].append(neighbor_penalty)
             print("\nInfo: Solusi optimal (penalti = 0) ditemukan. Menghentikan pencarian lebih awal.")
-            return current_state, current_penalty, history, history['iterasi'][-1]
+            return neighbor, neighbor_penalty, history, iterasi_total
         
         if neighbor_penalty < current_penalty:
             history['iterasi'].append(iterasi_total)
